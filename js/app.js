355e27a868bfd69823c1191ff199248fd948a209
norobbyrob <nrobinson18@punahou.edu>
2016-07-01 14:57:02 -1000

    modified

--- js/app.js
+++ js/app.js
@@ -1,14 +1,31 @@
 window.onload = doThings;
 
 function doThings(){
-}
-
-var hotLink = document.getElementById('Hot');
+	var 	 = document.getElementById('Hot');
 	hotLink.addEventListener('Hot', handleClick);
 
-var coolLink = document.getElementById('Cool');
-	coolLink.addEventListener('Hot', handleClick);
+	var coolLink = document.getElementById('Cool');
+		coolLink.addEventListener('Cool', handleClick);
 
-function handleClick(evt){
-  console.log(evt.target.id);
+	function handleClick(evt){
+	  console.log(evt.target.id);
+	}
+
+	//hoverVanTransform
+	var hoverVan = document.getElementById('Cool');
+	hoverVan.addEventListener('mouseover', handleHover);
+	hoverVan.addEventListener('mouseout', releaseHover);
+
+	var hoverVan = document.getElementById('Hot');
+	hoverVan.addEventListener('mouseover', handleHover);
+	hoverVan.addEventListener('mouseout', releaseHover);
+
+	function handleHover(evt){
+		 evt.target.className = 'ButtonHover';
+	}
+
+	function releaseHover(evt){
+		 evt.target.className = 'ButtonHover2';
+	}
 }
+
